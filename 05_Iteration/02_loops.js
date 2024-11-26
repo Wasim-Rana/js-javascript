86d4f7110ca6cfd144e3edfe9ec2b1313c636d1c
Wasim Rana <wasimrana2004@gmail.com>
2024-11-26 21:45:53 +0530

    loops

---
+++ 05_Iteration/02_loops.js
@@ -0,0 +1,36 @@
+// basic structure 
+
+// while (condition) {
+    
+// }
+
+let index = 0
+while(index <= 10){
+    // console.log(`value of index is ${index}`);
+    index += 3
+}
+
+let myArr =["mango","apple","banana"]
+let arr = 0
+while(arr < myArr.length){
+    // console.log(`value of index is ${myArr[arr]}`);
+    arr += 1 
+
+}
+
+// do while loop 
+
+
+// basic structure 
+
+// do {
+    
+// } while (condition);
+
+let score = 11
+do  { 
+        console.log(`score is ${score}`);
+        score ++
+        // it runs first and then check the condition even if your value is does not match the condition still it runs 
+        
+}   while(score <=10)
